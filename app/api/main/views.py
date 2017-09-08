216a9dc6277b957ecb88d418f1d8adcc1312714c
wangjingCN <289363142@qq.com>
2017-09-08 16:09:39 +0800

    [update]修改了教师页面

--- app/api/main/views.py
+++ app/api/main/views.py
@@ -3,6 +3,8 @@
 from flask import render_template
 from . import main
 from flask_login import login_required
+
+
 # from decorater import login_required
 
 
@@ -10,4 +12,10 @@ from flask_login import login_required
 # @login_required
 def index():
     return render_template('index.html')
+    # 需要写一个装饰器，判断是否登录，如果登录了，则跳到index,并生成对应的menu
+
+
+@main.route('/teacher', methods=['GET', 'POST'])
+def teacher_index():
+    return render_template('/teacher/index.html')
     # 需要写一个装饰器，判断是否登录，如果登录了，则跳到index,并生成对应的menu
